9f757531fd9f746b86a6706150c5f7d8bcf63f56
Aleks <aleksherla012@gmail.com>
2026-04-23 17:20:37 +0200

    poczatek canvas i pierwsza sciana labiryntu

---
+++ app.js
@@ -0,0 +1,8 @@
+let canvas_element=document.getElementById("canvas_gry");
+let canvas_context=canvas_element.getContext("2d");
+canvas_context.fillStyle="#FF0000";
+let x=50;
+let y =50;
+canvas_context.fillRect(x,y,50,50);
+console.log(canvas_element);
+
