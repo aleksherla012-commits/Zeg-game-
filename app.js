dd49790c5da0692ec3beac9ccf46c1d538deee9e
Aleks <aleksherla012@gmail.com>
2026-04-23 17:51:50 +0200

    wykrywanie klawiszy

--- app.js
+++ app.js
@@ -5,4 +5,28 @@ let x=50;
 let y =50;
 canvas_context.fillRect(x,y,50,50);
 console.log(canvas_element);
+document.addEventListener("keydown", function(event) {
 
+    if (event.key === "w" || event.key === "ArrowUp") {
+        y-=1;
+    }
+
+    if (event.key === "s" || event.key === "ArrowDown") {
+        y+=1;
+    }
+
+    if (event.key === "a" || event.key === "ArrowLeft") {
+        x-=1;
+    }
+
+    if (event.key === "d" || event.key === "ArrowRight") {
+        x+=1;
+    }
+
+    // czyszczenie canvasa
+    canvas_context.clearRect(0, 0, canvas_element.width, canvas_element.height);
+
+    // ponowne rysowanie
+    canvas_context.fillStyle = "#FF0000";
+    canvas_context.fillRect(x, y, 50, 50);
+});
