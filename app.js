0fec9f494c0a467f88fbf6078d6d1dcaaf57b4d7
Aleks <aleksherla012@gmail.com>
2026-04-24 21:53:05 +0200

    git commit poprawki kolejności labiryntu

--- app.js
+++ app.js
@@ -3,9 +3,45 @@ const canvas_context=canvas_element.getContext("2d");
 canvas_context.fillStyle="#FF0000";
 let x=50;
 let y =50;
-//wykrywanie klucza
+
+
+        cellSize = 50;
+cols = 16;
+rows = 12;
+const maze = [
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
+[1,0,1,1,0,1,0,1,1,1,1,1,0,0,1,0],
+[1,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0],
+[1,1,0,1,1,1,1,1,1,1,1,0,1,0,1,1],
+[1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1],
+[1,0,1,1,1,1,1,1,1,0,1,1,0,0,1,0],
+[1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
+[1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,0],
+[1,0,0,0,0,0,1,0,0,0,1,0,1,1,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,0,0,1,0],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
+for(i=0;i<rows;i++){
+    for(j=0;j<cols;j++){
+        let xCell=j*cellSize;
+        let yCell=i*cellSize;
+         canvas_context.strokeRect(xCell,yCell,cellSize,cellSize)
+         if (maze[i][j] == 1) {
+        canvas_context.fillStyle="black";
+        canvas_context.fillRect(xCell,yCell,cellSize,cellSize);
+
+}
+
+    }
+}
+    
+
+
+  //wykrywanie klucza
 canvas_context.fillRect(x,y,50,50);
 console.log(canvas_element);
+
 document.addEventListener("keydown", function(event) {
 //ruch 
     if (event.key === "w" || event.key === "ArrowUp") {
@@ -27,71 +63,12 @@ document.addEventListener("keydown", function(event) {
 
     // czyszczenie canvasa
     canvas_context.clearRect(0, 0, canvas_element.width, canvas_element.height);
+    
+        cellSize = 50;
 
-    cellSize = 50;
-cols = 16;
-rows = 12;
-const maze = [
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
-[1,0,1,1,0,1,0,1,1,1,1,1,0,0,1,0],
-[1,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0],
-[1,1,0,1,1,1,1,1,1,1,1,0,1,0,1,1],
-[1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1],
-[1,0,1,1,1,1,1,1,1,0,1,1,0,0,1,0],
-[1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
-[1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,0],
-[1,0,0,0,0,0,1,0,0,0,1,0,1,1,0,1],
-[1,0,1,1,1,0,1,1,1,0,1,1,0,0,1,0],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-];
-for(i=0;i<rows;i++){
-    for(j=0;j<cols;j++){
-        let x=j*cellSize;
-        let y=i*cellSize;
-         canvas_context.strokeRect(x,y,cellSize,cellSize)
-         if (maze[i][j] == 1) {
-        canvas_context.fillStyle="black";
-        canvas_context.fillRect(x,y,cellSize,cellSize);
-
-}
-
-    }
-}
-
-
-    // ponowne rysowanie
+})
+      // ponowne rysowanie
     canvas_context.fillStyle = "#FF0000";
     canvas_context.fillRect(x, y, 50, 50);
-});
 
-cellSize = 50;
-cols = 16;
-rows = 12;
-const maze = [
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
-[1,0,1,1,0,1,0,1,1,1,1,1,0,0,1,0],
-[1,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0],
-[1,1,0,1,1,1,1,1,1,1,1,0,1,0,1,1],
-[1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1],
-[1,0,1,1,1,1,1,1,1,0,1,1,0,0,1,0],
-[1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0],
-[1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,0],
-[1,0,0,0,0,0,1,0,0,0,1,0,1,1,0,1],
-[1,0,1,1,1,0,1,1,1,0,1,1,0,0,1,0],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-];
-for(i=0;i<rows;i++){
-    for(j=0;j<cols;j++){
-        let x=j*cellSize;
-        let y=i*cellSize;
-         canvas_context.strokeRect(x,y,cellSize,cellSize)
-         if (maze[i][j] == 1) {
-        canvas_context.fillStyle="black";
-        canvas_context.fillRect(x,y,cellSize,cellSize);
 
-}
-
-    }
-}
