3b0d2b776432e618455e7650f0942de161fa0118
Bartekk2422 <bartosz.skoczek22@gmail.com>
2026-04-26 15:11:16 +0200

    Uporzadkowanie kodu oraz dodanie interfejsu

---
+++ sounds.js
@@ -0,0 +1,13 @@
+// tworzy dźwięk(C)
+const audioCtx = new AudioContext();
+function playSound(frequency, duration) {
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    oscillator.frequency.value = frequency;
+    gainNode.gain.setValueAtTime(0.3, audioCtx.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + duration);
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + duration);
+}
