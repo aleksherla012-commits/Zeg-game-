75c6b90c3fd9cb19544838a1b3c874f6d719ab22
Bartekk2422 <bartosz.skoczek22@gmail.com>
2026-05-04 21:07:57 +0200

    Dodanie wiecej przedmiotow

--- player.js
+++ player.js
@@ -25,9 +25,19 @@ let highScore = 0;
 
 //przemioty na mapie 
 let items =[
-    {row: 3,col: 5, type:"heal",collected:false},
-    {row:7, col:12, type:"key", collected: false},
-    {row:11, col:7, type:"riddle", collected:false},
+    {row: 3,  col: 5,  type:"heal",   collected:false},
+    {row: 1,  col: 22, type:"key",    collected:false, keyId:1},
+    {row: 9,  col: 15, type:"key",    collected:false, keyId:2},
+    {row: 5,  col: 5,  type:"key",    collected:false, keyId:3},
+    {row: 11, col: 7,  type:"riddle", collected:false},
 ];
+
+// drzwi: każde wymaga konkretnego klucza
+const doors = [
+    {row:3,  col:11, keyId:1, open:false},
+    {row:10, col:3,  keyId:2, open:false},
+    {row:16, col:11, keyId:3, open:false},
+];
+
 let riddleSolved=false;
 let gameOver=false ;
